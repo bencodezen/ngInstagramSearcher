974098667395a8e8f21801d167f95a3df7f658c8
Ben Hong <bencodezen@gmail.com>
2015-10-08 17:36:56 -0400

    [F] Add Ajax request to Instagram API

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 angular.module('instaSearcherApp', [])
-	.controller('instaSearcherCtrl', function($scope) {
+  .controller('instaSearcherCtrl', function($scope, $http) {
+    var url = "https://api.instagram.com/v1/tags/puppy/media/recent";
+    var request = {
+      "client_id": "ca6875003ac547a9a3bafff119ffb863",
+      count: 10,
+      callback: 'JSON_CALLBACK'
+    };
 
-	});
+    $http({
+      method: 'JSONP',
+      url: url,
+      params: request
+    })
+    .then(function(result) {
+      console.log(result);
+    })
+    .then(function(error) {
+      console.log(error);
+    });
+  });
